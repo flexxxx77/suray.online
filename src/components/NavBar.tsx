d9a6b3897b65be1f5477543e2b3fc6394e552371
Пүрэвсүрэн Дэмбэрэл <purevsuren@coremind.mn>
2025-04-13 20:24:41 +0800

    white background

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex items-center justify-between px-4 py-3 bg-white dark:bg-black border-b border-gray-300 dark:border-white fixed top-0 z-50">
+    <nav className="w-full flex items-center justify-between px-4 py-3 bg-white border-b border-gray-300 fixed top-0 z-50">
       <Link href="/" className="flex items-center">
         <Image
           src="/logo.jpg"
@@ -14,10 +14,10 @@ export default function Navbar() {
         />
       </Link>
       <div className="flex items-center space-x-6 text-sm font-medium">
-        <Link href="/about" className="hover:underline text-black dark:text-white">
+        <Link href="/about" className="hover:underline text-black">
           About Us
         </Link>
-        <Link href="/feedback" className="hover:underline text-black dark:text-white">
+        <Link href="/feedback" className="hover:underline text-black">
           Санал хүсэлт
         </Link>
       </div>
