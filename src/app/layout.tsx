b75b4e80e2c5bcfc0fa6116030fb525d189dba50
Пүрэвсүрэн Дэмбэрэл <purevsuren@coremind.mn>
2025-04-15 22:11:05 +0800

    so many changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
-"use client";
-
 import "./globals.css";
 import { Metadata } from "next";
 import Script from "next/script";
 import Navbar from "@/components/NavBar";
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
-import { pageview } from "@/app/lib/gtag";
+import ClientWrapper from "@/components/ClientWrapper";
 
 export const metadata: Metadata = {
   title: "Suray.online",
@@ -18,18 +14,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
-  useEffect(() => {
-    if (pathname) {
-      pageview(pathname);
-    }
-  }, [pathname]);
-
   return (
     <html lang="mn" suppressHydrationWarning>
       <head>
-        {/* 📊 Google Analytics */}
+        {/* 📊 Google Analytics */} 
         <Script
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-RMX1N907MW"
@@ -50,6 +38,9 @@ export default function RootLayout({
       <body className="scroll-smooth transition-colors duration-300 bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
         {/* 🔝 Navbar */}
         <Navbar />
+
+        {/* 🌐 Google Analytics pageview tracking */}
+        <ClientWrapper />
 
         {/* ⚡ Background Gradient */}
         <div className="absolute inset-0 -z-10 bg-gradient-to-br from-yellow-100 via-pink-100 to-white dark:from-gray-800 dark:via-gray-900 dark:to-gray-950" />
